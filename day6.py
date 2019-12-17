52bca83bba78bf7f24079485048ffdaf8f40f4fe
Erik van Raalte <erik.vanraalte94@gmail.com>
2019-12-17 22:55:08 +0100

    add 6-2

--- day6.py
+++ day6.py
@@ -1,7 +1,8 @@
-def findDeps(orbit,relations):
+def findDeps(orbit,relations,lst):
     for r in relations:
         if orbit in relations:
-            return 1 + findDeps(relations[orbit],relations)
+            lst.append((orbit,relations[orbit]))
+            return 1 + findDeps(relations[orbit],relations,lst)
     return 0
 
 
@@ -18,8 +19,42 @@ unique_orbits = list(dict.fromkeys(flat_orbits))
 orbit_relations = {sub[1] : sub[0] for sub in orbit_relations}
 
 part1 = 0
+
+you = []
+san = []
+
 for orbit in unique_orbits:
-    deps = findDeps(orbit,orbit_relations)
-    # print(f"{orbit}: {deps}")
+    lst = []
+    deps = findDeps(orbit,orbit_relations,lst)
+    # print(lst)
+
+    def contains(lst,string):
+        for e in lst:
+            if e[0] == string:
+                return True
+        return False
+
+    if contains(lst,'YOU'):
+        you = lst[:]
+    
+    if contains(lst,'SAN'):
+        san = lst[:]
+
     part1 += deps
+
+
+# print(f"you : {you}")
+# print(f"san : {san}")
+
+# print(f"difference : {set(san).difference(set(you))}")
+# print(f"difference : {set(you).difference(set(san))}")
+
+youCrossingLen = len(set(san).difference(set(you))) - 1
+sanCrossingLen = len(set(you).difference(set(san))) - 1
+part2 = youCrossingLen + sanCrossingLen
+
+
+# part2 = len(you.union(san) - you.difference(san)) - 1
+
 print(f"part: {part1}")
+print(f"part: {part2}")
