311d64c2459a3a09c98949b29d31f0a21b6a0e81
Erik van Raalte <erik.vanraalte94@gmail.com>
2019-12-21 11:37:28 +0100

    improve scalability

--- day10.py
+++ day10.py
@@ -16,22 +16,28 @@ for y, line in enumerate(lines):
     for x,char in enumerate(line):
         if char == '#':
             asteroids.append(Asteroid(x,y))
-maxLos = 0
-for base in asteroids:
-    me = id(base)
-    angles = []
-    for a in asteroids:
-        if not id(a) == me:
-            dx = a.x - base.x
-            dy = a.y - base.y
-            angle = math.degrees(math.atan2(dx,dy))
-            # print(f"base ({base.x},{base.y}), a ({a.x},{a.y}) => {angle}")
-            angles.append(angle)
-    los = len(list(dict.fromkeys(angles)))
-    base.los = los
-    maxLos = los if  los > maxLos else maxLos
 
-print(maxLos)
-# for base in asteroids:
-#     if base.los == maxLos:
-#         print(f"({base.x},{base.y})")
+def bestBaseStation(asteroids):
+    best = asteroids[0]
+    for base in asteroids:
+        me = id(base)
+        angles = []
+        for a in asteroids:
+            if not id(a) == me:
+                dx = a.x - base.x
+                dy = a.y - base.y
+                angle = math.degrees(math.atan2(dx,dy))
+                # print(f"base ({base.x},{base.y}), a ({a.x},{a.y}) => {angle}")
+                angles.append(angle)
+        los = len(list(dict.fromkeys(angles)))
+        base.los = los
+        best = base if base.los > best.los else best
+    return best
+
+base = bestBaseStation(asteroids)
+print(f"Part 1: ({base.x},{base.y}) => {base.los}")
+
+
+# for a in asteroids:
+#     if a.los == maxLos:
+#         print(f"({a.x},{a.y})")
