4c8b21757cb8d2d0e6e4550800b5bc617b9c4e04
Erik van Raalte <erik.vanraalte94@gmail.com>
2019-12-21 12:21:49 +0100

    add 10-2

--- day10.py
+++ day10.py
@@ -18,31 +18,48 @@ for y, line in enumerate(lines):
         if char == '#':
             asteroids.append(Asteroid(x,y))
 
+def determineLos(base,asteroids):
+    me = id(base)
+    los = []
+    for a in asteroids:
+        if not id(a) == me:
+            dx = a.x - base.x 
+            dy = base.y - a.y 
+            angle = math.degrees(math.atan2(dx,dy))
+            angle = angle + 360 if angle < 0 else angle
+            los.append((angle,a.x,a.y))    
+    seen = set()
+    los = [(a,b,c) for a,b,c in los if not (a in seen or seen.add(a))]
+    los = sorted(los,key = lambda x: x[0])
+    return los
+
 def bestBaseStation(asteroids):
     best = asteroids[0]
     for base in asteroids:
-        me = id(base)
-        los = []
-        for a in asteroids:
-            if not id(a) == me:
-                dx = a.x - base.x 
-                dy = base.y - a.y 
-                angle = math.degrees(math.atan2(dx,dy))
-                angle = angle + 360 if angle < 0 else angle
-                los.append((angle,a.x,a.y))
-
-        seen = set()
-        los = [(a,b,c) for a,b,c in los if not (a in seen or seen.add(a))]
-        los = sorted(los,key = lambda x: x[0])
-        base.los = los
-        base.numLos = len(los)
+        base.los = determineLos(base,asteroids)
+        base.numLos = len(base.los)
         best = base if base.numLos > best.numLos else best
     return best
 
+num = 0
+
+def vaporize(base,asteroids):
+    global num
+    for l in base.los:
+        (_,x,y) = l
+        for a in asteroids:
+            if a.x == x and a.y == y:
+                num += 1
+                if num == 200:
+                    print(f"Part 2: the {num}th asteroid to be vaporized is at ({a.x},{a.y}) or {100*a.x + a.y}")
+                asteroids.remove(a)
+                
 base = bestBaseStation(asteroids)
 print(f"Part 1: ({base.x},{base.y}) => {base.numLos}")
 
-# print(base.los)
-# for a in asteroids:
-#     if a.los == maxLos:
-#         print(f"({a.x},{a.y})")
+
+while len(asteroids) > 1:
+    # print(len(asteroids))
+    vaporize(base,asteroids)
+    base.los = determineLos(base,asteroids)
+    base.numLos = len(base.los)
