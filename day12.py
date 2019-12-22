82a60215c7eb8bd57d1b07319e78fd67370134af
Erik van Raalte <erik.vanraalte94@gmail.com>
2019-12-22 19:46:51 +0100

    add 12-1

--- day12.py
+++ day12.py
@@ -3,38 +3,35 @@ from itertools import combinations
 import numpy as np
 
 
-def calcVelocities(velocities,positions):
-    v = velocities[:]
-    
-    np.greater()
-    # for d in range(0,3):
+def calc(v,positions):
     for c in combinations([0,1,2,3],2):
-        np.greater(positions[c[0]],positions[c[1]])
-        # p0 = positions[c[0]][d]
-        # p1 = positions[c[1]][d]
-        # print(f"p0: {p0}, p1: {p1}")      
-        # if p1 > p0:
-        #     v[c[0]][d] += 1
-        #     v[c[1]][d] -= 1                 
-        # elif p1 < p0:
-        #     v[c[0]][d] -= 1
-        #     v[c[1]][d] += 1
-        # print(v)   
-    print(v)
-    return v
-        
-        
+        p0 = positions[c[0]]
+        p1 = positions[c[1]]
+        v0 = velocities[c[0]]
+        v1 = velocities[c[1]]
 
+        v0 = np.where(p0 > p1, v0 - 1, np.where(p0 < p1, v0 + 1, v0)) 
+        v1 = np.where(p1 > p0, v1 - 1, np.where(p1 < p0, v1 + 1, v1)) 
+
+        velocities[c[0]] = v0
+        velocities[c[1]] = v1
+        pass
+    kin = np.sum(np.abs(v),axis=1)
+
+    return (v,kin)
+        
+        
 f = open("day12.input","r")
 positionsRaw =  f.readlines()
 
-positions = list(map(lambda x: list(map(int,re.sub("<|>| |\n|x|y|z|=","",x).split(","))),positionsRaw))
-# print(positions)
-velocities = [[0]*3]*4
+positions = np.array(list(map(lambda x: list(map(int,re.sub("<|>| |\n|x|y|z|=","",x).split(","))),positionsRaw)))
 
-for _ in range(0,10):
-    velocities = calcVelocities(velocities,positions)   
-    # print(velocities) 
-    # positions = list(map(list,list(np.add(velocities,positions))))
-    # print(positions)
+velocities = np.array([[0]*3]*4)
+
+print(positions)
+for i in range(1,1000+1):
+    velocities,kinetic = calc(velocities,positions)
+    positions = velocities + positions
+    potential = np.sum(np.abs(positions),axis=1)  
+print(f"Total energy: {np.sum(potential * kinetic)}")
 
